05c0840e12c309d9d6fc70a257ced0d5f58043c9
bowornnan <47479280+bowornnan@users.noreply.github.com>
2020-03-12 16:02:30 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'AmhOo6K9lNMcSn6rRhj8dwAe/ZO+hrF09hSUsayIxZr2kuVwJJ/qDK+wzP9DrMfRnD5YamgaXXwgUAvIoP2Fmuj5zPgvjfidZu9vRbBcleCtdDtlzYPsHa7X7b+2LQuqrhvFENCG3lsHcl/ym29uiAdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U2872871a6f0c52cf748a1bd5c9f7e940',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
